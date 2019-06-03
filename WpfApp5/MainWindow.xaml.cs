948ec00be8819675828cc83a03a4453a3504fea2
sadiev <sadiev@itstep.kz>
2019-06-03 18:59:43 +0600

    Добавьте файлы проекта.

---
+++ WpfApp5/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace WpfApp5
+{
+  /// <summary>
+  /// Логика взаимодействия для MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+
+      var allProcess =  Process.GetProcesses();
+ 
+      int i = 0;
+      foreach (var proc in allProcess)
+      {
+        i++;
+        string infoproc = string.Format($"{proc.Id},{proc.StartTime},{proc.MachineName}");
+        datagrid.ItemsSource = infoproc;
+      }
+ 
+    }
+
+    private void button_Click(object sender, RoutedEventArgs e)
+    {
+      
+    }
+  }
+}
